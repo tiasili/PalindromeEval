a01dcd1f3de1131134ddbac4580561cc53ea86e6
tiasili <43101714+tiasili@users.noreply.github.com>
2019-02-15 10:22:19 -0800

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,29 +1,28 @@
 //Tia Silivelio
-//LLQ.h
-//Purpose: The LLQ class declares functions for the main function (P2.cpp) to use. It
-//also declares variables to be used for the functions in the LLQ implementation.
+//Queue.h
+//Purpose: The Queue class declares functions for the main function (main.cpp) to use. It
+//also declares variables to be used for the functions in the Queue implementation.
 
-#ifndef LLQ_H
-#define LLQ_H
+#ifndef QUEUE_H
+#define QUEUE_H
 #include <iostream>
 
 using namespace std;
 
-class LLQ
+class Queue
 {
  public:
-  LLQ();
-  ~LLQ();
+  Queue();
+  ~Queue();
   void enQueue(int d);
   int deQueue();
   //pre-condition: in order to dequeue integers, queue cannot be empty
   bool isEmpty() const;
   void clear(); //resets values in queue
   int getSize(); //gets size of linked list
-  void flush(); //removes negative integers from queue
  private:
-  LLQ(const LLQ& q); //function is suppressed in private
-  void operator=(const LLQ& q);
+  Queue(const Queue& q); //function is suppressed in private
+  void operator=(const Queue& q);
 
   struct qNode //node used to traverse through linked list
   {
